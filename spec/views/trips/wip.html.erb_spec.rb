e7a41e4361c3b7caee5cbc4efd701a89923ef933
Dan Steinicke <dansteinicke@gmail.com>
2010-08-13 10:39:02 -0700

    Fix WIP header test an make it non-pending

--- spec/views/trips/wip.html.erb_spec.rb
+++ spec/views/trips/wip.html.erb_spec.rb
@@ -1,12 +1,12 @@
-require 'spec_helper' 
+require 'spec_helper'
 
 describe "trips/wip.html.erb" do
   before(:each) do
-    @vehicles = (1..7).collect {|i| "car#{i}"}
+    @vehicles = (1..7).collect { |i| "car#{i}" }
     assigns[:vehicles] = @vehicles
     @start_date = (Date.today - Date.today.wday.days).strftime("%Y%m%d")
     assigns[:start_date] = @start_date
-    @days = (0..6).collect {|i| Date.parse(@start_date) + i.days}
+    @days = (0..6).collect { |i| Date.parse(@start_date) + i.days }
     assigns[:days] = @days
     @trips_by_hour = mock('trips_by_hour_mock')
     @trips_by_hour.stub("has_hour?").and_return("class=\"car1-trip\"")
@@ -16,29 +16,23 @@ describe "trips/wip.html.erb" do
   describe "WIP table" do
 
     it "should render the navbar template" do
-      template.should_receive(:render).with( :partial => "navbar",
-        :locals => {:start_date => @start_date, :cal_type => 'wip'})
+      template.should_receive(:render).with(:partial => "navbar",
+                                            :locals => {:start_date => @start_date, :cal_type => 'wip'})
       render
     end
 
     it "should have two header rows with days of the week and dates" do
-      pending
       render
-      response.should have_selector('table', :id => 'wip') do |wip|
-        [1,2].each do |row|
-          wip.should have_selector("tr:nth-child(#{row})", :id => 'wip-header') do |header|
-            (1..7).each do |i|
-              month_day = @days[i-1].strftime("%b %d")
-              wk_day = @days[i-1].strftime("%a")
-              year = @days[i-1].strftime("%Y")
-              markup = "#{month_day} #{wk_day} #{year}"
-              header.should contain(markup)
-            end
-          end
+      response.should have_selector("tr", :id => 'wip-header', :count => 2) do |header|
+        week_start = Date.today - Date.today.wday.days
+        week_days = (0..6).collect { |n| week_start + n.days }
+        week_days.each do |day|
+          formatted_date = day.strftime("%b %d")
+          header.should have_tag('span', :text=>formatted_date)
         end
       end
     end
-  
+
     it "should have a subheader for each vehicle under each day" do
       render
       response.should have_selector('table', :id => 'wip') do |wip|
@@ -52,11 +46,11 @@ describe "trips/wip.html.erb" do
 
     it "should show hours along left side" do
       hours = ["12:00AM", "12:30AM", "1:00AM", "1:30AM", "2:00AM", "2:30AM", "3:00AM", "3:30AM",
-        "4:00AM", "4:30AM", "5:00AM", "5:30AM", "6:00AM", "6:30AM", "7:00AM", "7:30AM", "8:00AM",
-        "8:30AM", "9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM",
-        "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM",
-        "5:00PM", "5:30PM", "6:00PM", "6:30PM", "7:00PM", "7:30PM", "8:00PM", "8:30PM", "9:00PM",
-        "9:30PM", "10:00PM", "10:30PM", "11:00PM", "11:30PM"]
+               "4:00AM", "4:30AM", "5:00AM", "5:30AM", "6:00AM", "6:30AM", "7:00AM", "7:30AM", "8:00AM",
+               "8:30AM", "9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM",
+               "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM",
+               "5:00PM", "5:30PM", "6:00PM", "6:30PM", "7:00PM", "7:30PM", "8:00PM", "8:30PM", "9:00PM",
+               "9:30PM", "10:00PM", "10:30PM", "11:00PM", "11:30PM"]
       render
       response.should have_selector('table', :id=>'wip') do |wip|
         (1..24).each do |row_num|
@@ -95,7 +89,7 @@ describe "trips/wip.html.erb" do
   it "should have a title 'WIP for date' " do
     y, m, d = Date.parse(@start_date).strftime("%Y %B %d").split
     render
-    response.should contain( "Vehicle use for week beginning Sunday #{m} #{d.to_i.ordinalize} #{y}")
+    response.should contain("Vehicle use for week beginning Sunday #{m} #{d.to_i.ordinalize} #{y}")
   end
-  
+
 end
