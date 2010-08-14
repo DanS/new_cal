4e6325b34709972a32af0497aacbc6a82518f595
Dan Steinicke <dansteinicke@gmail.com>
2010-08-13 17:14:06 -0700

    Add fancy bouncing blob navbar stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,26 @@
 $(document).ready(function(){
   $('#banner h1').css("fontSize", "6px");
   $('#banner h1').animate({fontSize:"28px"}, 1000);
+
+//create background blob for navbar
+$('<div id="nav-blob"></div>').css({
+		width: $('#navigation li:first a').width() + 10,
+		height: $('#navigation li:first a').height() + 20
+}).appendTo('#navbar').hide();
+
+$('#navbar a').hover(function(){
+    // Mouse over function
+	  $('#nav-blob').animate(
+      {width: $(this).width() + 10, left: $(this).position().left},
+	    {duration: 'slow', easing: 'easeOutElastic', queue: false}
+    );
+  }, function() {
+	  // Mouse out function
+	  var leftPosition = $('#navbar a:first').position().left;
+	  $('#nav-blob').animate(
+      {width:'hide'},
+			{duration:'slow', easing: 'easeOutCirc', queue:false}
+		).animate({left: leftPosition}, 'fast' );
+  });
+
 });
