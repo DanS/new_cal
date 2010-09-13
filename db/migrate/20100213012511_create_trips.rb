90e79d313481370655524a740cd19797690af61d
Dan Steinicke <dansteinicke@gmail.com>
2010-09-13 16:39:24 -0700

    Add indexes to trips table.  Clean up vehicle table migrations.

--- db/migrate/20100213012511_create_trips.rb
+++ db/migrate/20100213012511_create_trips.rb
@@ -13,6 +13,9 @@ class CreateTrips < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    add_index :trips, :date
+    add_index :trips, :destination
   end
 
   def self.down
--- db/migrate/20100224190843_create_vehicles.rb
+++ db/migrate/20100224190843_create_vehicles.rb
@@ -1,7 +1,10 @@
 class CreateVehicles < ActiveRecord::Migration
   def self.up
-    create_table :vehicles do |t|
-      t.string :name
+    create_table "vehicles", :force => true do |t|
+      t.string   "name"
+      t.string   "color"
+      t.string   "firm_assignment"
+      t.string   "not_dr_vehicle"
 
       t.timestamps
     end
