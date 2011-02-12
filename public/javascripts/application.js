8724b7202b11b13be0d941341a3917e33359a438
Dan Steinicke <dansteinicke@gmail.com>
2011-02-11 21:24:06 -0800

    CSS changes to navbar, new trip button, wip view, fix javascript selectors broken by previous css changes.  Add js to select current view in navbar.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,8 @@
 var windowWidth;
+windowWidth = $(window).width();
+$(window).resize(function() {
   windowWidth = $(window).width();
-  $(window).resize(function() {
-    windowWidth = $(window).width();
-  });
+});
 
 //add tooltips to the month calendar
 var TT = {
@@ -39,7 +39,7 @@ var TT = {
         })
       }
     });
-    TT.popupHeader = $('table.trip-list tr:first').clone();
+    TT.popupHeader = $('div#trip-list tr:first').clone();
     TT.popupHeader.find('th:nth-child(6)').html('Vehicle').end();//shorten column name
     TT.popupHeader.prependTo('table.popup');
   }
@@ -74,7 +74,7 @@ var showOnly = function(letter, dest) {
     }
   });
   //trip-list
-  $('table.trip-list tr.trip').each(function(i, e) {
+  $('div#trip-list tr.trip').each(function(i, e) {
     if ($(e).find('td:nth-child(3)').attr('class') != letter) {
       $(e).hide()
     }
@@ -98,7 +98,7 @@ var showAll = function() {
   // week calendar
   $('table.day td.week-trip').show();
   //trip-list
-  $('table.trip-list tr.trip').show();
+  $('div#trip-list tr.trip').show();
 };
 
 //var setTripListWidth = function() {
@@ -108,88 +108,89 @@ var showAll = function() {
 //};
 
 $(document).ready(function() {
-    //set tooltips on month view
-    TT.setTips();
+  //set tooltips on month view
+  TT.setTips();
 
-    //add date picker to trip form
-    Date.firstDayOfWeek = 0;
-    Date.format = 'mm/dd/yyyy';
-    $('#trip_date').datepicker();
+  //add date picker to trip form
+  Date.firstDayOfWeek = 0;
+  Date.format = 'mm/dd/yyyy';
+  $('#trip_date').datepicker();
 
-    //set click action on destination list
-    $('div#destination-list a').css('text-decoration', 'none').click(function(e) {
-      e.preventDefault();
+  //set click action on destination list
+  $('div#destination-list a').css('text-decoration', 'none').click(function(e) {
+    e.preventDefault();
+  });
+  $('div#destination-list table td').each(function(i, e) {
+    var letter = $(this).attr('class');
+    var dest = $(this).find('a span').html();
+    var that = $(this);
+    $(this).click(function() {
+      $('div#destination-list table td a').removeClass('selected');
+      that.find('a').addClass('selected');
+      showOnly(letter, dest);
     });
-    $('div#destination-list table td').each(function(i, e) {
-      var letter = $(this).attr('class');
-      var dest = $(this).find('a span').html();
-      var that = $(this);
-      $(this).click(function() {
-        $('div#destination-list table td a').removeClass('selected');
-        that.find('a').addClass('selected');
-        showOnly(letter, dest);
-      });
-      $('div#destination-list table tr:last').click(function() {
-        showAll(letter);
-      });
-
-//    setTripListWidth();
-
-      setTimeout(function() {
-        // expand the title on page open
-        $('#banner h1').css("fontSize", "6px");
-        $('#banner h1').animate({fontSize:"28px"}, 1000);
-      }, 50);
-
+    $('div#destination-list table tr:last').click(function() {
+      showAll(letter);
     });
 
-    //WIP accordion
-    var openCloseDiv = function(openDiv, closeDiv) {
-      if (openDiv != null) {
-        openDiv.addClass('active').stop(true).animate({width: '450px'}, {queue:false, duration:400})
-            .addClass('active').css({'text-align': 'center', 'background-color':'transparent'})
-            .find('th.vehicle-header').css('font-size', 'x-small').end().find('span, a').show();
-      }
-      if (closeDiv != null) {
-        $.each(closeDiv, function(i, colDiv) {
-          var divWidth = $(colDiv).find('.time-header').length > 0 ? '100px' : '45px'
-          $(colDiv).removeClass('active').stop(true).animate({width: divWidth}, {queue:false, duration:400})
-              .removeClass('active').css({'text-align': 'left', 'background-color': 'gray'})
-              .find('th.vehicle-header').css('font-size', '0').end().find('span, a').hide();
-        });
-        $('.hour, .time-header').css({'background-color': 'white'});
-      }
-    };
+    // highlight the nav menu selection for the current page
+    if ($('#month-calendars').size() > 0) { $('a#month-view').css({'color':'black', 'text-decoration':'underline'}) }
+    if ($('#week_calendar').size() > 0) {$('a#week-view').css({'color':'black', 'text-decoration':'underline'}) }
+    if ($('table#wip').size() > 0) {$('a#wip-view').css({'color':'black', 'text-decoration':'underline'})}
 
-    //start with current day open
-    openCloseDiv(null, $("table#wip div[class^='col']"));
-    var weekDayNum = new Date().getDay() + 1;
-    openCloseDiv($("table#wip div.col" + weekDayNum), null);
-
-    $('[class^=col]').hover(function() {
-      if ($(this).hasClass('active')) {
-        return;  //do nothing when over open div
-      }
-      var currentDiv = $(this);
-      currentDiv.addClass('waiting');
-      setTimeout(function() {
-        if (currentDiv.hasClass('waiting')) {
-          openCloseDiv(currentDiv, $('table#wip div.active'));
-          currentDiv.removeClass('waiting');
-        }
-      }, 400)
-    },
-                           function() {
-                             $(this).removeClass('waiting')
-                           });
-    //have only every 4th row border solid in WIP table
-    $('table#wip table').each(function() {
-      $(this).find('tr').each(function(i) {
-        if ((i + 3) % 4 == 0) {
-          $(this).css("border-bottom", "1px solid black")
-        }
-      })
-    })
+    // expand the title on page open
+    $('#banner h1').css("fontSize", "6px");
+    $('#banner h1').animate({fontSize:"28px"}, 1000);
 
   });
+
+  //WIP accordion
+  var openCloseDiv = function(openDiv, closeDiv) {
+    if (openDiv != null) {
+      openDiv.addClass('active').stop(true).animate({width: '450px'}, {queue:false, duration:400})
+          .addClass('active').css({'text-align': 'center', 'background-color':'transparent'})
+          .find('th.vehicle-header').css('font-size', 'x-small').end().find('span, a').show();
+    }
+    if (closeDiv != null) {
+      $.each(closeDiv, function(i, colDiv) {
+        var divWidth = $(colDiv).find('.time-header').length > 0 ? '100px' : '45px'
+        $(colDiv).removeClass('active').stop(true).animate({width: divWidth}, {queue:false, duration:400})
+            .removeClass('active').css({'text-align': 'left', 'background-color': 'gray'})
+            .find('th.vehicle-header').css('font-size', '0').end().find('span, a').hide();
+      });
+      $('.hour, .time-header').css({'background-color': 'white'});
+    }
+  };
+
+  //start with current day open
+  openCloseDiv(null, $("table#wip div[class^='col']"));
+  var weekDayNum = new Date().getDay() + 1;
+  openCloseDiv($("table#wip div.col" + weekDayNum), null);
+
+  $('[class^=col]').hover(function() {
+    if ($(this).hasClass('active')) {
+      return;  //do nothing when over open div
+    }
+    var currentDiv = $(this);
+    currentDiv.addClass('waiting');
+    setTimeout(function() {
+      if (currentDiv.hasClass('waiting')) {
+        openCloseDiv(currentDiv, $('table#wip div.active'));
+        currentDiv.removeClass('waiting');
+      }
+    }, 400)
+  },
+                         function() {
+                           $(this).removeClass('waiting')
+                         });
+  //have only every 4th row border solid in WIP table
+  $('table#wip table').each(function() {
+    $(this).find('tr').each(function(i) {
+      if ((i + 3) % 4 == 0) {
+        $(this).find('td').css("border-bottom", "1px solid black")
+      }
+    })
+  })
+
+});
 
